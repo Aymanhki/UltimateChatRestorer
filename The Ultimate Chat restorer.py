5aaab5f5259810d7ea4e299273ec0077fbfcdd68
Ayman Agamy <agamyahk@myumanitoba.ca>
2025-03-28 23:07:26 -0500

    Added support for media files

--- The Ultimate Chat restorer.py
+++ The Ultimate Chat restorer.py
@@ -1,13 +1,13 @@
 import json
+import os
+import zipfile
+import sys
 
 def convert_telegram_to_whatsapp(telegram_export_path, whatsapp_output_path):
-    # Load the Telegram export JSON file
     with open(telegram_export_path, 'r', encoding='utf-8') as file:
         telegram_data = json.load(file)
 
-    # Open the output file where the WhatsApp-like chat will be saved
     with open(whatsapp_output_path, 'w', encoding='utf-8') as output_file:
-        # Check if 'messages' is directly in the root or under 'chats'
         if 'messages' in telegram_data:
             messages = telegram_data['messages']
         elif 'chats' in telegram_data:
@@ -17,31 +17,71 @@ def convert_telegram_to_whatsapp(telegram_export_path, whatsapp_output_path):
         else:
             messages = []
 
-        # Iterate through each message in the Telegram export
         for message in messages:
-            # Extract the necessary information
             date = message.get('date', '')
             from_user = message.get('from', '')
             text = message.get('text', '')
+            media_type = message.get('media_type', '')
+            mine_type = message.get('mime_type', '')
+            file_url = message.get('file', '')
 
-            # WhatsApp doesn't use the exact same date format, but we'll simplify it
-            # Telegram format: 2020-01-01T00:00:00
-            # Simplified for our purposes: [01/01/2020, 00:00:00]
             date_parts = date.split('T')
             if len(date_parts) == 2:
                 date_formatted = date_parts[0].replace('-', '/')
                 time_formatted = date_parts[1]
                 date = f"[{date_formatted}, {time_formatted}]"
 
-            # Prepare the message in a WhatsApp-like format
-            whatsapp_message = f"{date} - {from_user}: {text}\n"
+            if isinstance(text, str):
+                whatsapp_message = f"{date} - {from_user}: {text}\n"
+            else:
+                for text_object in text:
+                    if isinstance(text_object, dict):
+                        text_from_text_dict_object = text_object.get('text', '')
+                        whatsapp_message = f"{date} - {from_user}: {text_from_text_dict_object}\n"
+                    elif isinstance(text_object, str):
+                        whatsapp_message = f"{date} - {from_user}: {text_object}\n"
 
-            # Handle non-string messages (e.g., media messages)
-            if not isinstance(text, str):
-                whatsapp_message = f"{date} - {from_user}: <Media omitted>\n"
-
-            # Write the formatted message to the output file
             output_file.write(whatsapp_message)
 
-# Calling the function
-convert_telegram_to_whatsapp('result.json', '_chat.txt')
+            if file_url:
+                # output_file.write(f"{date} - {from_user}: <attached: {file_url}>\n")
+                filename = os.path.basename(file_url)
+                output_file.write(f"{date} - {from_user}: <attached: {filename}>\n")
+
+
+def zipdir(path, ziph):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file),
+                       os.path.relpath(os.path.join(root, file), os.path.join(path, '..')))
+
+def create_zip_file():
+    zipf = zipfile.ZipFile('Whatsapp Chat - xxx.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipf.write('_chat.txt')
+    media_folders = ['photos', 'stickers', 'video_files', 'voice_messages', 'round_video_messages', 'files', 'contacts']
+
+    for folder in media_folders:
+        if os.path.exists(folder):
+            # zipdir(folder, zipf)
+            for root, dirs, files in os.walk(folder):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    zipf.write(file_path, arcname=os.path.basename(file_path))
+
+    zipf.close()
+
+def main():
+    if len(sys.argv) > 1:
+        telegram_export_path = sys.argv[1]
+        whatsapp_output_path = sys.argv[2]
+    else:
+        telegram_export_path = 'result.json'
+        whatsapp_output_path = '_chat.txt'
+
+    convert_telegram_to_whatsapp(telegram_export_path, whatsapp_output_path)
+    create_zip_file()
+
+if __name__ == "__main__":
+    main()
+
+
